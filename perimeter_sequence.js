7aeb35165b9f85507f32b182d1f235d3c55b4247
Vkube <sawmati@gmail.com>
2022-01-09 02:19:06 +0300

    feat: add removeChar

---
+++ perimeter_sequence.js
@@ -0,0 +1,9 @@
+//Perimeter sequence
+//The first three stages of a sequence are shown.
+
+function perimeterSequence(a, n) {
+  return a * n * 4;
+}
+
+console.log(perimeterSequence(1, 3), 12);
+export default perimeterSequence;
