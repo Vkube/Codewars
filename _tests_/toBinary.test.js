b75bdd3392cdb454705b8337dc5de3db51c54fbf
Vkube <sawmati@gmail.com>
2022-01-09 03:02:11 +0300

    feat: add toBinary.test

---
+++ _tests_/toBinary.test.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@jest/globals';
+import {toBinary} from "../tobinary";
+
+
+test('toBinary', () => {
+    expect(toBinary(1)).toEqual(1);
+  // expect(toBinary(2).toEqual(10));
+  });
