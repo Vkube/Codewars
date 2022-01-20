35b7fdd365866889237dcf6b2ced9e0ec130425c
vkube <sawmati@gmail.com>
2022-01-21 02:04:21 +0300

    refactor: bundle file

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,16 @@
-(()=>{"use strict";var e={};function o(e,o){return 180-(e+o)}(e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e),console.log(o(30,60),90),console.log(o(60,60),60),(0,e.otherAngle)()})();
+(() => {
+  "use strict";
+  var e = {};
+  function o(e, o) {
+    return 180 - (e + o);
+  }
+  ((e) => {
+    "undefined" != typeof Symbol &&
+      Symbol.toStringTag &&
+      Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+      Object.defineProperty(e, "__esModule", { value: !0 });
+  })(e),
+    console.log(o(30, 60), 90),
+    console.log(o(60, 60), 60),
+    (0, e.otherAngle)();
+})();
