90d72c5de665c8e1ce74d2cbf47a7643904f8603
Vkube <sawmati@gmail.com>
2022-04-23 14:47:44 +0300

    Merge commit '08a83bb2c48ff1c6dc19d390e56dc9e146e5f7b6'

---
+++ digits_explosion.js
@@ -0,0 +1,15 @@
+//Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value. 
+
+//explode("312")
+//should return :
+//"333122"
+
+function explode(s) {
+    let arr = s.split(" ");
+    
+    
+    return "";
+  }
+
+
+  console.log(explode('312'),'333122');
