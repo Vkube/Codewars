69b3b828d81eb5fd573b9184ffadcf6f104ff545
Vkube <sawmati@gmail.com>
2022-04-23 14:51:12 +0300

    add new method bit to decimal

---
+++ C#/Bit to Decimal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var test = Program.binToDec("11");
+        }
+
+
+        public static int binToDec(string s)
+        {
+            return Convert.ToInt32(s, 2);
+        }
+    }
+}
