2a96401bff777c3ec0945499cd47652e26ca6329
Vkube <sawmati@gmail.com>
2022-09-01 17:20:37 +0300

    add new method Ones and Zero

---
+++ C#/7kyu/Ones and Zeros/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Ones_and_Zeros
+{
+    public class Kata
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            string numbers = "";
+            for (int i = 0; i < BinaryArray.Length; i++)
+            {
+                numbers += BinaryArray[i].ToString();
+            }
+
+            return Convert.ToInt32(numbers, 2);
+        }
+    }
+}
---
+++ C#/7kyu/Ones and Zeros/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ones_and_Zeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 1, 1, 1 };
+
+            var result = Kata.binaryArrayToNumber(array);
+            Console.WriteLine(result);
+        }
+    }
+}
