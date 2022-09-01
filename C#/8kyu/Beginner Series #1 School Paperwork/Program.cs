db7b14584c7d734ebbf0c2ff81d8f0bb856e33db
Vkube <sawmati@gmail.com>
2022-09-01 17:21:09 +0300

    add new method sum of Paper

---
+++ C#/8kyu/Beginner Series #1 School Paperwork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beginner_Series__1_School_Paperwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Paper.Paperwork(5, 5));
+        }
+        
+        
+    }
+
+    public class Paper
+    {
+        public static int Paperwork(int n, int m)
+        {
+            if(n < 0 || m < 0 ) return 0;
+      
+            int result = n * m;
+            return result;
+        }
+    }
+}
