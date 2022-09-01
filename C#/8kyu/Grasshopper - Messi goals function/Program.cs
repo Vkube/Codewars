551b1b8df4b9a0140bab67776953c168d4fae082
Vkube <sawmati@gmail.com>
2022-09-01 16:53:15 +0300

    add new solution

---
+++ C#/8kyu/Grasshopper - Messi goals function/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Grasshopper___Messi_goals_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetGoals(1, 4, 5));
+        }
+    }
+
+    public class Kata
+    {
+        public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+        {
+            return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+        }
+    }
+}
