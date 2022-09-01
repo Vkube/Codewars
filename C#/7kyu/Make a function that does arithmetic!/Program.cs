033f9f272c8bf8e1a949b7ea6590c39cf264c680
Vkube <sawmati@gmail.com>
2022-09-01 17:37:40 +0300

    add new solution Make a function that does arithmetic!

---
+++ C#/7kyu/Make a function that does arithmetic!/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Make_a_function_that_does_arithmetic_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Arithmetic(1, 2, "add"));
+        }
+    }
+    
+    public class Kata 
+    {
+        public static double Arithmetic(double a, double b, string op)
+        {
+            double result=0; 
+            switch (op)
+            {
+                case "add":
+                    result = a + b;
+                    return result;
+                    
+                case "subtract":
+                    result = a - b;
+                    return result;
+                  
+                case "multiply":
+                    result = a * b;
+                    return result;
+                  
+                case "divide":
+                    result = a / b;
+                    return result;
+                 
+                default:
+                    return result;
+            }
+            
+        }
+    }
+}
