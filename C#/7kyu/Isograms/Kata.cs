79d93094c02f16abb67f2aaf4d588671eebf2949
Vkube <sawmati@gmail.com>
2022-09-01 18:03:55 +0300

    add new method isogram

---
+++ C#/7kyu/Isograms/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Isograms
+{
+    public class Kata
+    {
+        public static bool IsIsogram(string str)
+        {
+            str = str.ToLower();
+            string uniqCharacter = new String(str.Distinct().ToArray());
+            return str == uniqCharacter;
+        }
+    }
+}
