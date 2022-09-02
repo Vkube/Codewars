81402d45ffaa73d5289824866f5400a93baf79c7
Vkube <sawmati@gmail.com>
2022-09-02 13:41:23 +0300

    add CountBy X

---
+++ C#/8kyu/Count by X/Kata.cs
@@ -0,0 +1,14 @@
+﻿namespace Count_by_X
+{
+    public static class Kata
+    {
+        public static int[] CountBy(int x, int n)
+        {
+            int[] z = new int[n];
+            for (int k = 0; k < n; k++) {
+                z[k] = (k + 1) * x;
+            }
+            return z;
+        }
+    }
+}
---
+++ C#/8kyu/Count by X/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Count_by_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
