0d575f2862e93ec0c0b365d28ade46be4319f601
Vkube <sawmati@gmail.com>
2022-09-02 14:16:15 +0300

    add new solution Twice as old

---
+++ C#/8kyu/Twice as old/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Twice_as_old
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var result= TwiceAsOldSolution.TwiceAsOld(45, 30);
+           Console.WriteLine(result);
+        }
+    }
+    
+    public class TwiceAsOldSolution
+    {
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            int ageOfSun = sonYears * 2;
+            int result = dadYears - ageOfSun;
+
+            return Math.Abs(result);
+        }
+    }
+}
+
