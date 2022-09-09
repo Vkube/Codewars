a57af7c2d8612b36f8b85091c0b5c1bec5c5271c
Vkube <sawmati@gmail.com>
2022-09-09 12:25:10 +0300

    reverse words

---
+++ C#/8kyu/Reversed Words/Kata.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+namespace Reversed_Words
+{
+    public class Kata
+    {
+        public static string ReverseWords(string str)
+        {
+            return (string)str.Reverse();
+        }
+    }
+}
---
+++ C#/8kyu/Reversed Words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reversed_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.ReverseWords("hello world!"));
+        }
+    }
+}
