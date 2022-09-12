d76a9ec62715c13b37facd8317178ab4a45d3402
Vkube <sawmati@gmail.com>
2022-09-12 20:12:58 +0300

    add between

---
+++ C#/8kyu/What is between/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace What_is_between
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Kata.Between(-2, 2);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    
+
+
+    public class Kata {
+        public static int[] Between(int a, int b)
+        {
+            List<int> array = new List<int>();
+
+            for (int i = a; i <= b; i++)
+            {
+                array.Add(i);
+            }
+
+            return array.ToArray();
+        }
+    }
+}
