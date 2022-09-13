d8d54cda456c46712291fe309e0ca2d63cb7e167
Vkube <sawmati@gmail.com>
2022-09-13 15:18:15 +0300

    add List Filtering

---
+++ C#/7kyu/List Filtering/ListFilterer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Filtering
+{
+    public class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return listOfItems.OfType<int>();
+        }
+    }
+}
---
+++ C#/7kyu/List Filtering/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Filtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>(){1,"a","b",0,15};
+           var result= ListFilterer.GetIntegersFromList(list);
+           foreach (var item in result)
+           {
+               Console.WriteLine(item);
+           }
+        }
+    }
+}
