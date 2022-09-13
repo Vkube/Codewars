15075318ea1cc8281fc90d49fae47fa421efba28
Vkube <sawmati@gmail.com>
2022-09-13 15:18:53 +0300

    Is it even

---
+++ C#/8kyu/Is it even/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Is_it_even
+{
+    class Number
+    {
+        static void Main(string[] args)
+        {
+            IsEven(12);
+        }
+
+        private static bool IsEven(double n) => n % 2 == 0;
+    }
+}
