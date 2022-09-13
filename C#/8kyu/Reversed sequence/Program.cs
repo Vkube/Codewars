23379d36b3b82de0559e9787dec08758688098e0
Vkube <sawmati@gmail.com>
2022-09-13 15:19:07 +0300

    Reversed sequence

---
+++ C#/8kyu/Reversed sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Reversed_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result=Kata.ReverseSeq(5);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    
+
+
+    public static class Kata
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            var result = Enumerable.Range(1,n).Reverse().ToArray();
+            return result;
+        }
+    }
+}
