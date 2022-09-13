bcbb0618802051e570d893e9b21750041421105a
Vkube <sawmati@gmail.com>
2022-09-13 15:17:51 +0300

    add Multiples of 3 or 5

---
+++ C#/6kyu/Multiples of 3 or 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Multiples_of_3_or_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Solution(10));
+        }
+    }
+
+
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            var sumOfNumbers = new List<int>();
+
+            if (value < 0)
+                return 0;
+
+            for (int i = 0; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumOfNumbers.Add(i);
+                }
+            }
+
+            return sumOfNumbers.Sum();
+        }
+    }
+}
