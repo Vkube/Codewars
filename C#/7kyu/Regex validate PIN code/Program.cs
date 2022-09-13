7a4c5f885a752c5748e2cc62c92f6d0a8e181fea
Vkube <sawmati@gmail.com>
2022-09-13 15:18:37 +0300

    Regex validate PIN code

---
+++ C#/7kyu/Regex validate PIN code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Regex_validate_PIN_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.ValidatePin("1");
+        }
+    }
+
+
+    public class Kata
+    {
+        public static bool ValidatePin(string pin)
+        {
+            var condition = !string.IsNullOrEmpty(pin)
+                            && (pin.Length == 4 || pin.Length == 6)
+                            && pin.All(char.IsDigit);
+            return condition;
+        }
+    }
+}
