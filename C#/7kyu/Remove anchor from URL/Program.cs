a2dcd4b9903c6efcef4a5f5901810c9633372ba0
Vkube <sawmati@gmail.com>
2022-10-01 02:43:01 +0300

    Remove anchor from URL

---
+++ C#/7kyu/Remove anchor from URL/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Remove_anchor_from_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result=Kata.RemoveUrlAnchor("www.codewars.com/katas/?page=1#about");
+        }
+    }
+    
+    
+    
+
+public static class Kata
+{
+  public static string RemoveUrlAnchor(string url)
+  {
+      if (url.Contains('#'))
+      {
+          int indexOfSymbol= url.IndexOf('#');
+              string newUrl = url.Substring(0,indexOfSymbol);
+              return newUrl;
+      }
+
+      return url;
+  }
+}
+}
