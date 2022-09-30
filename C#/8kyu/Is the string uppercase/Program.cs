05e06e66871b00cf1df94a2aeb34b10b35a3e1de
Vkube <sawmati@gmail.com>
2022-10-01 02:43:51 +0300

    Is the string uppercase

---
+++ C#/8kyu/Is the string uppercase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Is_the_string_uppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public static class StringExtensions
+    {
+        public static bool IsUpperCase(this string text)
+        {
+            string upperString = text.ToUpper();
+            return text == upperString;
+        }
+    }
+}
