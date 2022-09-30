28fc4046c1ed2edb53c4b81ee6e01e46359c137c
Vkube <sawmati@gmail.com>
2022-10-01 02:44:50 +0300

    diagonalDifference

---
+++ HackerRank/DiagonalDifference/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<List<int>>() {new List<int>(){1,2,3,4}, new List<int>(){1,23,4,5}};
+            var firstList = array[0][0];
+        }
+        
+        /*
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+
+
+            var array = new List<List<int>>() {new List<int>(){1,2,3,5}};
+            var firstList = array[0];
+            
+
+
+
+        }
+        */
+        
+    }
+}
