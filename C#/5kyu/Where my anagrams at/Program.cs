850da1424fbf304edc2dd6afb7de5d9ac6352998
Vkube <sawmati@gmail.com>
2022-10-01 02:40:58 +0300

    add Anagrams

---
+++ C#/5kyu/Where my anagrams at/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Where_my_anagrams_at
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayOfWords = new List<string> { "a", "b", "c", "d" };
+            Kata.Anagrams("a", arrayOfWords);
+        }
+    }
+    
+    public static class Kata
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            var listOfWords = new List<string>();
+            var charOfArrays = words.Select(c => c.ToCharArray());
+            
+            //
+            return listOfWords;
+        }
+    }
+    
+    
+    
+    
+}
