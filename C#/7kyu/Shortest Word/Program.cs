db218849deacf9d087bd0383be2d4fd408c32b20
Vkube <sawmati@gmail.com>
2022-10-01 02:42:43 +0300

    Shortest Word

---
+++ C#/7kyu/Shortest Word/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shortest_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res1 = Kata.FindShort("bitcoin take over the world maybe who knows perhaps");
+            var res2 = Kata.FindShort("turns out random test cases are easier than writing out basic ones");
+            var res3 = Kata.FindShort("Let's travel abroad shall we");
+        }
+    }
+    
+    public class Kata
+    {
+        public static int FindShort(string s)
+        {
+            /*string[] words = s.Split(' ');
+            List<int> lengthWords = new List<int>();
+
+            foreach (var word in words)
+            {
+                lengthWords.Add(word.Length);
+            }
+
+            return lengthWords.Min();*/
+            
+            var words = s.Split(' ');
+            var sorted=words.OrderBy(n => n.Length);
+            var shortest = sorted.FirstOrDefault();
+            return shortest.Length;
+        }
+    }
+}
