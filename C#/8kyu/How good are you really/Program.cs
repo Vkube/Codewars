64f52edc8c292c1cd3edf219163a492b8cdbb0e0
Vkube <sawmati@gmail.com>
2022-10-01 02:43:19 +0300

    How good are you really

---
+++ C#/8kyu/How good are you really/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace How_good_are_you_really
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classAverage = new int[] { 100, 40, 34, 57, 29, 72, 57, 88 };
+            var result = Kata.BetterThanAverage(classAverage, 75);
+        }
+    }
+    
+    public class Kata
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            return ClassPoints.Average() < YourPoints;
+        }
+    }
+}
