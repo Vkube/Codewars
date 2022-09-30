d8f8974957b41202fa34d2aaa1afb11edf2eb6db
Vkube <sawmati@gmail.com>
2022-10-01 02:41:36 +0300

    Find the next perfect square!

---
+++ C#/7kyu/Find the next perfect square!/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace Find_the_next_perfect_square_
+{
+    public class Kata
+    {
+        public static long FindNextSquare(long num)
+        {
+            var o = (long)Math.Sqrt((double)num);
+            return !BigInteger.Pow(o, 2).Equals(num) ? -1 : (long)BigInteger.Pow(o + 1, 2);
+           
+        }
+    }
+}
---
+++ C#/7kyu/Find the next perfect square!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_the_next_perfect_square_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result=Kata.FindNextSquare(244808216190636484);
+        }
+    }
+}
