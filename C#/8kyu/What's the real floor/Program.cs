4cfecd86470a963eb47d471f7fd38f68f967992a
Vkube <sawmati@gmail.com>
2022-10-01 02:44:36 +0300

    What's the real floor

---
+++ C#/8kyu/What's the real floor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace What_s_the_real_floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res1 = Kata.GetRealFloor(1);
+            var res2 = Kata.GetRealFloor(5);
+            var res3 = Kata.GetRealFloor(15);
+        }
+    }
+    
+    public static class Kata
+    {
+        public static int GetRealFloor(int n)
+        {
+            if (n < 0 || n==0)
+            {
+                return n;
+            }
+            else if (n > 13)
+            {
+                return n -= 2;
+            }
+            else if (n <= 13)
+            {
+                return n -= 1;
+            }
+
+            return n;
+        }
+    }
+}
